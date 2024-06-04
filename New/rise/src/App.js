f4c93ed931d3d6ab1a24162f64c8ecbf6f4fccee
Lucas Garcia <lucas.garcia@sptech.school>
2024-06-03 22:24:45 -0300

    fix(login/cadastros) - fixed the responsivity and the integration with the Vlibras api

--- New/rise/src/App.js
+++ New/rise/src/App.js
@@ -1,13 +1,17 @@
-import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { ToastContainer } from 'react-toastify';
 import Rotas from "./routes";
 
+
 function App() {
   return (
-    <>
+    <React.StrictMode>
       <Rotas />
       <ToastContainer />
-    </>
+    </React.StrictMode>
   );
 }
 export default App;
--- New/rise/src/pages/cadastros/instituteRegistration/InstituteRegistration.jsx
+++ New/rise/src/pages/cadastros/instituteRegistration/InstituteRegistration.jsx
@@ -9,12 +9,12 @@ import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import VLibras from "@djpfs/react-vlibras";
 
 const InstituteRegistration = () => {
-    const [showSecondForm, setShowSecondForm] = useState(false);
+    const [secondFormVisible, setSecondFormVisible] = useState(false);
 
-    const handleContinue = () => {
-        console.log("Continuar clicado");
-        setShowSecondForm(true);
+    const showSecondForm = () => {
+        setSecondFormVisible(true);
     };
+
 
     return (
         <>
@@ -29,30 +29,48 @@ const InstituteRegistration = () => {
                         <div>
                             <div className={`${styles["form-presentation"]}`}>
                                 <FontAwesomeIcon icon={faArrowRightToBracket} style={{ color: '#000000' }} />
-                                <label>{showSecondForm ? "Informe os dados do representante do instituto" : "Cadastro de instituto"}</label>
+                                <label>{secondFormVisible ? "Informe os dados do representante do instituto" : "Cadastro de instituto"}</label>
                             </div>
-                            <label className={`${styles["standard-text"]}`}>{showSecondForm ? "Informe os dados do representante do instituto" : "Informe os dados da sua intituição"}</label>
+                            <label className={`${styles["standard-text"]}`}>{secondFormVisible ? "Informe os dados do representante do instituto" : "Informe os dados da sua intituição"}</label>
                         </div>
 
-                        <div className={`${styles["container-inputs-form"]}`} style={{ display: showSecondForm ? 'none' : 'block' }}>
+                        <div className={`${styles["container-inputs-form"]} col-md-12`} style={{ display: secondFormVisible ? 'none' : 'block' }}>
                             <LabelInput placeholder={"Digite a razão social"} label={"Razão social"} />
                             <LabelInput placeholder={"Digite seu CNPJ"} label={"CNPJ"} />
                             <LabelInput placeholder={"Digite o CEP"} label={"CEP"} />
                             <LabelInput placeholder={"Digite o número do estabelecimento"} label={"Número do estabelecimento"} />
+                            <br />
 
-                            <BlueButton txt={showSecondForm ? "Cadastrar" : "Continuar"} onClick={handleContinue} />
+                            <div className={`${styles['low-form']}`}>
+                                <div className="form-group form-check">
+                                    <input type="checkbox" className="form-check-input" id="lembrarCheck"></input>
+                                    <label className="form-check-label" htmlFor="lembrarCheck">Lembre de mim</label>
+                                </div>
+                                <a href="#" className="forgot-password-link">esqueci minha senha</a>
+                            </div>
                         </div>
 
-                        <div className={`${styles['container-inputs-form']}`} style={{ display: showSecondForm ? 'block' : 'none' }}>
+
+                        <div className={`${styles['container-inputs-form']}`} style={{ display: secondFormVisible ? 'block' : 'none' }}>
                             <LabelInput placeholder={"Digite seu nome"} label={"Nome"} />
                             <LabelInput placeholder={"Digite seu CPF"} label={"CPF"} />
                             <LabelInput placeholder={"Digite seu endereço"} label={"Endereço"} />
                             <LabelInput placeholder={"Digite seu e-mail"} label={"E-mail"} type="email" />
                             <LabelInput placeholder={"Digite sua senha"} label={"Senha"} type="password" />
                             <LabelInput placeholder={"Confirme sua senha"} label={"Confirmação de senha"} type="password" />
+                            <br></br>
 
-                            <BlueButton txt={"Cadastrar"} />
+                            <div className={`${styles['low-form']}`}>
+                                <div className="form-group form-check">
+                                    <input type="checkbox" className="form-check-input" id="lembrarCheck"></input>
+                                    <label className="form-check-label" htmlFor="lembrarCheck">Lembre de mim</label>
+                                </div>
+                                <a href="#" className="forgot-password-link">esqueci minha senha</a>
+                            </div>
+
                         </div>
+
+                        <BlueButton txt={secondFormVisible ? "Cadastrar" : "Continuar"} onclick={showSecondForm} />
                     </div>
                 </div>
             </div>
