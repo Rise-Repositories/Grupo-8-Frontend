2157c70ae139c94ac429ca8e762a9f457931b227
danielyuzo <daniel.palma@sptech.school>
2024-06-06 00:29:13 -0300

    fix: unused import

--- New/rise/src/components/heatmap/Heatmap.jsx
+++ New/rise/src/components/heatmap/Heatmap.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, FC } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
