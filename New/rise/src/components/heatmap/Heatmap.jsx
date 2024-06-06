974621ecbe340afdb61be1c7579c7da5ce246043
danielyuzo <daniel.palma@sptech.school>
2024-06-05 23:22:46 -0300

    fix: changing heatmap library

--- New/rise/src/components/heatmap/Heatmap.jsx
+++ New/rise/src/components/heatmap/Heatmap.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { HeatmapLayer } from 'react-leaflet-heatmap-layer-v3';
+import 'leaflet.heat';
 import api from '../../api';
 import { toast } from 'react-toastify';
+
+function HeatmapData(props) {
+    const heatmapOptions = {
+        max: 1.0,
+        maxZoom: 15,
+        radius: 50,
+        gradient: { 0.2: 'blue', 0.6: 'yellow', 1.0: 'red' }
+    };
+    console.log(props.pontos);
+    const map = useMap();
+    L.heatLayer(props.pontos, heatmapOptions).addTo(map);
+}
 
 const Heatmap = () => {
 
@@ -13,8 +26,8 @@ const Heatmap = () => {
     useEffect(() => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((curPosition) => {
-                console.log(curPosition);
                 setPosition([curPosition.coords.latitude, curPosition.coords.longitude]);
+            
             }, (error) => {
                 console.error("Erro ao obter a localização: ", error);
             });
@@ -22,16 +35,16 @@ const Heatmap = () => {
             console.error("Geolocalização não é suportada pelo navegador.");
         }
 
-        let heatmapConfig = {
+        const requestConfig = {
             params: {
                 radiusToGroup: 100.0,
                 olderThan: '2024-06-04T00:00:00'
             }
         };
 
-        api.get('/mapping/heatmap', heatmapConfig).then((res) => {
-            console.log(res);
+        api.get('/mapping/heatmap', requestConfig).then((res) => {
             setPontosMapaCalor(res.data);
+
         }).catch((err) => {
             toast.error('Erro ao carregar dados do mapa de calor');
         });
@@ -42,22 +55,14 @@ const Heatmap = () => {
             <MapContainer
                 center={position || [-23.5581592, -46.6614485]}
                 zoom={position || 13}
-                style={{ height: "80vh", width: "100%" }}
+                style={{ height: "100%", width: "100%" }}
             >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
 
-                <HeatmapLayer
-                    fitBoundsOnLoad
-                    fitBoundsOnUpdate
-                    points={pontosMapaCalor}
-                    latitudeExtractor={m => m[0]}
-                    longitudeExtractor={m => m[1]}
-                    intensityExtractor={m => m[2]} 
-                    radius={50}
-                    gradient={ {0.2: 'blue', 0.6: 'orange', 1.0: 'red'} } />
+                <HeatmapData pontos={pontosMapaCalor} />
             </MapContainer>
         </>
     );
