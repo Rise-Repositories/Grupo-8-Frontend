19a48993bf170062d774ac52dd8396cff9be1187
Lucas Garcia <lucas.garcia@sptech.school>
2024-06-07 18:48:51 -0300

    fix(navbar vertical) fixed the svg icons

--- New/rise/src/components/navbar/navbarVertical/NavbarVertical.jsx
+++ New/rise/src/components/navbar/navbarVertical/NavbarVertical.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import styles from './NavbarVertical.module.css';
 import { Link } from 'react-router-dom';
 
+import riseLogo from '../../../utils/imgs/rise-logo.png';
+import logo from '../../../utils/imgs/logo.png';
+
 const NavbarVertical = () => {
     return (
         <>
-            <nav className={styles.sidebar}>
+            <nav className={`col-md-2 ${styles["sidebar"]}`}>
                 <div className={styles.container}>
                     <div className={styles.upPart}>
                         <h3>
@@ -26,6 +29,14 @@ const NavbarVertical = () => {
                             </a>
                             <a className={styles.scrollLink} href="#">
                                 Dashboard
+                            </a>
+                        </li>
+                        <li className={styles.links}>
+                            <a className={styles.scrollLink} href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#ffffff" d="M565.6 36.2C572.1 40.7 576 48.1 576 56V392c0 10-6.2 18.9-15.5 22.4l-168 64c-5.2 2-10.9 2.1-16.1 .3L192.5 417.5l-160 61c-7.4 2.8-15.7 1.8-22.2-2.7S0 463.9 0 456V120c0-10 6.1-18.9 15.5-22.4l168-64c5.2-2 10.9-2.1 16.1-.3L383.5 94.5l160-61c7.4-2.8 15.7-1.8 22.2 2.7zM48 136.5V421.2l120-45.7V90.8L48 136.5zM360 422.7V137.3l-144-48V374.7l144 48zm48-1.5l120-45.7V90.8L408 136.5V421.2z" /></svg>
+                            </a>
+                            <a className={styles.scrollLink} href="#">
+                                Mapeamento
                             </a>
                         </li>
                         <li className={styles.links}>
@@ -62,11 +73,12 @@ const NavbarVertical = () => {
                         </li>
                     </ul>
 
-                    <div className={styles.logos}>
-                        <h3>
-                            <img src="./image-removebg-preview 1.png" alt="" width="30px" height="30px" /> | <img src="../../img/img.png" alt="" width="30px" height="30px" />
-                        </h3>
+                    <div className={styles["logos"]}>
+                        <img src={riseLogo} alt="Rise Logo" />
+                        |
+                        <img src={logo} alt="Logo" />
                     </div>
+
                 </div>
             </nav>
         </>
