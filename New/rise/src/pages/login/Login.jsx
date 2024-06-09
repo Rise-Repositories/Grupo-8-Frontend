5e94ee86f836192bf95d81a5d82c2fe52d6c3ff8
GabrielBifon05 <gabriel.bsouza@sptech.school>
2024-06-09 19:22:24 -0300

    feat: login backend integration

--- New/rise/src/pages/login/Login.jsx
+++ New/rise/src/pages/login/Login.jsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import styles from "./Login.module.css";
 import NavBar from "../../components/navbar/navbarHorizontal/NavbarHorizontal";
 import LabelInput from "../../components/inputs/labelInput/LabelInput";
 import BlueButton from "../../components/buttons/blueButton/BlueButton";
 import backgroundImage from "../../utils/imgs/maos-dadas.png";
+import api from "../../api";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { AuthContext } from "./AuthContext";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 import VLibrasComponent from "../../components/vlibras/VlibrasComponent";
 
-
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleInputChange = (event, setStateFunction) => {
+        setStateFunction(event.target.value);
+    }
+
+    const  entrar = () => {
+        let config = {
+            headers: {
+                "Content-type": "application/json"
+            }
+        }
+        let body = {
+            email: email,
+            password: senha
+        }
+
+        api.post('/user/auth/login', body, config)
+            .then((respostaObtida) => {
+                
+                const token = respostaObtida.data.token;
+                login(token);
+                toast.success("Login efetuado com sucesso")
+                sessionStorage.setItem("USER_TOKEN", respostaObtida.data.token)
+                navigate('../dashboard');
+            })
+            .catch((erroOcorrido) => {
+                console.log(erroOcorrido)
+                toast.error("Email ou senha incorretos")
+            })
+    }
+
     return (
         <>
             <NavBar />
@@ -30,8 +69,8 @@ const Login = () => {
                         </div>
 
                         <div className={`${styles["container-inputs-form"]}`}>
-                            <LabelInput placeholder={"Digite seu e-mail"} label={"E-mail"} />
-                            <LabelInput placeholder={"Digite sua senha"} label={"Senha"} />
+                            <LabelInput placeholder={"Digite seu e-mail"} label={"E-mail"} onInput={(e) => handleInputChange(e, setEmail)}/>
+                            <LabelInput placeholder={"Digite sua senha"} label={"Senha"} onInput={(e) => handleInputChange(e, setSenha)}/>
 
                             <div className={`${styles['low-form']}`}>
                                 <div className="form-group form-check">
@@ -42,7 +81,7 @@ const Login = () => {
                             </div>
                         </div>
 
-                        <BlueButton txt={"Entrar"} />
+                        <BlueButton onclick={entrar} txt={"Entrar"} />
                     </div>
                 </div>
             </div>
--- New/rise/src/routes.js
+++ New/rise/src/routes.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
@@ -6,20 +5,27 @@ import VolunteerRegistration from "./pages/cadastros/volunteerRegistration/volun
 import InstituteRegistration from "./pages/cadastros/instituteRegistration/InstituteRegistration";
 import Dashboard from "./pages/dashboard/Dashboard";
 import DashboardMapping from "./pages/dashboardMapping/DashboardMapping";
+import PrivateRoute from "./pages/login/PrivateRoute";
 
 function Rotas() {
     return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/volunteer-registration" element={<VolunteerRegistration />} />
-                    <Route path="/institute-registration" element={<InstituteRegistration />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/dashboard-mapping" element={<DashboardMapping />} />
-                </Routes>
-            </BrowserRouter>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/volunteer-registration" element={<VolunteerRegistration />} />
+                <Route path="/institute-registration" element={<InstituteRegistration />} />
+                <Route path="/dashboard" element={
+                    <PrivateRoute>
+                        <Dashboard />
+                    </PrivateRoute>
+                } />
+                <Route path="/dashboard-mapping" element={
+                    <PrivateRoute>
+                        <DashboardMapping />
+                    </PrivateRoute>
+                } />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
