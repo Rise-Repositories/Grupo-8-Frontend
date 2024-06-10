24011818268ce7ccba842f0950947924a7ef200c
danielyuzo <daniel.palma@sptech.school>
2024-06-10 01:11:24 -0300

    fix: change wrong input messages

--- New/rise/src/utils/globals.js
+++ New/rise/src/utils/globals.js
@@ -1,8 +1,39 @@
-function formatDate(dataString) {
+export function formatDate(dataString) {
     const data = new Date(dataString);
     const dia = data.getDate().toString().padStart(2, '0');
     const mes = (data.getMonth() + 1).toString().padStart(2, '0');
     const ano = data.getFullYear();
     return `${dia}/${mes}/${ano}`;
 }
-export default formatDate;
+
+export function validateText(text) {
+    return text.trim() ? true : false;
+}
+
+export function validateCPF(cpf) {
+    const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    return regex.test(cpf);
+};
+
+export function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+};
+
+export function validatePassword(password) {
+    return /[0-9]/.test(password) &&
+        /[a-z]/.test(password) &&
+        /[A-Z]/.test(password) &&
+        /[@#$%&*-+!?~^`_<>()[\]{}\/\\]/.test(password) &&
+        password.length >= 6
+}
+
+export function validateCNPJ(cnpj) {
+    const regex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
+    return regex.test(cnpj);
+};
+
+export function validateCEP(cep) {
+    const regex = /^\d{5}-\d{3}$/;
+    return regex.test(cep);
+};
