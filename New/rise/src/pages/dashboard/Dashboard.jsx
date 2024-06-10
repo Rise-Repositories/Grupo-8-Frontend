1633ab5295f3d6408d480b13aa2481b95a43d8ed
GabrielBifon05 <gabriel.bsouza@sptech.school>
2024-06-10 19:38:51 -0300

    refactor: updating kpi percentage

--- New/rise/src/pages/dashboard/Dashboard.jsx
+++ New/rise/src/pages/dashboard/Dashboard.jsx
@@ -65,9 +65,9 @@ const Dashboard = () => {
                 qtyServed: responseKpis.data.qtyServed,
                 qtyNotServed: responseKpis.data.qtyNotServed,
                 qtyNoPeople: responseKpis.data.qtyNoPeople,
-                qtyServedPercent: responseKpis.data.qtyServed / responseKpis.data.qtyTotal,
-                qtyNotServedPercent: responseKpis.data.qtyNotServed  / responseKpis.data.qtyTotal,
-                qtyNoPeoplePercent: responseKpis.data.qtyNoPeople / responseKpis.data.qtyTotal
+                qtyServedPercent: (responseKpis.data.qtyServed / responseKpis.data.qtyTotal),
+                qtyNotServedPercent: (responseKpis.data.qtyNotServed  / responseKpis.data.qtyTotal),
+                qtyNoPeoplePercent: (responseKpis.data.qtyNoPeople / responseKpis.data.qtyTotal)
             });
         } catch (error) {
             console.error('Erro ao buscar dados', error);
@@ -184,7 +184,7 @@ const Dashboard = () => {
                                     <div className={styles["iconKPI"]}>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#2968c8" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM160 152c0-13.3 10.7-24 24-24h88c44.2 0 80 35.8 80 80c0 28-14.4 52.7-36.3 67l34.1 75.1c5.5 12.1 .1 26.3-11.9 31.8s-26.3 .1-31.8-11.9L268.9 288H208v72c0 13.3-10.7 24-24 24s-24-10.7-24-24V264 152zm48 88h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H208v64z" /></svg>
                                     </div>
-                                    <div className="valueKPI">{kpis.qtyNotServed} <span className={styles["valueKpiPercent"]}>({kpis.qtyNotServed}%)</span></div>
+                                    <div className="valueKPI">{kpis.qtyNotServed} <span className={styles["valueKpiPercent"]}>({kpis.qtyNotServedPercent}%)</span></div>
                                     <div className="titleKPI">Total de locais não atendidos</div>
                                 </div>
                                 <div className={`col-md-2 ${styles["standardKPIDark"]}`}>
