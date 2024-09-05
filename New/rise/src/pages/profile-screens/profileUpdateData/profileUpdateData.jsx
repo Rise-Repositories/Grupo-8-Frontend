576100e53a1843d64c6a1a002e3950cc4a125d38
npz12 <nicolas.zanardi@sptech.school>
2024-09-05 20:08:30 -0300

    fix:body styling

--- New/rise/src/pages/profile-screens/profileUpdateData/profileUpdateData.jsx
+++ New/rise/src/pages/profile-screens/profileUpdateData/profileUpdateData.jsx
@@ -5,42 +5,44 @@ import { faCircleChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 function ProfileUpdateData() {
   return (
-    <div className={styles["profile-edit"]}>
-      <div className={styles["container"]}>
+        <div className={"col-12 col-md-5 mx-md-auto"} >
+    <div className={styles["container"]}>
         <header>
           <button className={styles.backButton} onClick={() => window.history.back()}>
             <FontAwesomeIcon icon={faCircleChevronLeft} style={{ color: "#1a3e95" }} />
           </button>
           <h1>Editar Perfil</h1>
         </header>
+        <form>
+          <div className={styles["aux"]}>
+            <div className={styles["input-group"]}>
+              <label htmlFor="name">Nome:</label>
+              <input type="text" id="name" placeholder="Nome Exemplo" />
+            </div>
+            <div className={styles["input-group"]}>
+              <label htmlFor="email">E-mail:</label>
+              <input type="email" id="email" placeholder="exemplo@gmail.com" />
+            </div>
+            <div className={styles["input-group"]}>
+              <label htmlFor="cpf">CPF:</label>
+              <input type="text" id="cpf" placeholder="XXX.XXX.XXX-XX" />
+            </div>
+            <div className={styles["input-group"]}>
+              <label htmlFor="address">Endereço:</label>
+              <input type="text" id="address" placeholder="Rua Exemplo N°X" />
+            </div>
+            <div className={styles["input-group"]}>
+              <label htmlFor="address">Nova Senha:</label>
+              <input type="password" id="address" placeholder="" />
+            </div>
+            <div className={styles["input-group"]}>
+              <label htmlFor="address">Confirmar nova senha:</label>
+              <input type="password" id="address" placeholder="" />
+            </div>
+            <button type="submit" className={styles["update-button"]}>Atualizar</button>
+          </div>
+        </form>
       </div>
-      <form>
-        <div className={styles["input-group"]}>
-          <label htmlFor="name">Nome:</label>
-          <input type="text" id="name" placeholder="Nome Exemplo" />
-        </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="email">E-mail:</label>
-          <input type="email" id="email" placeholder="exemplo@gmail.com" />
-        </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="cpf">CPF:</label>
-          <input type="text" id="cpf" placeholder="XXX.XXX.XXX-XX" />
-        </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="address">Endereço:</label>
-          <input type="text" id="address" placeholder="Rua Exemplo N°X" />
-        </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="address">Nova Senha:</label>
-          <input type="password" id="address" placeholder="" />
-        </div>
-        <div className={styles["input-group"]}>
-          <label htmlFor="address">Confirmar nova senha:</label>
-          <input type="password" id="address" placeholder="" />
-        </div>
-        <button type="submit" className={styles["update-button"]}>Atualizar</button>
-      </form>
     </div>
   );
 }
